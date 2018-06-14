cdb98e280b7f7e2d1c2320ca5b60a168b6b80bef
dcorral3 <dcorral@opennebula.org>
2018-06-14 16:09:15 +0200

    Authentication for answering only to specific chat_id

--- myserverbot.py
+++ myserverbot.py
@@ -4,32 +4,38 @@ import sys
 import time
 import telepot
 import config
-import commands 
+import commands
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+def auth(chat_id):
+    return chat_id == config.chad_id
 
 def c_handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Hostname',
-                       callback_data='hostname'), 
-                    InlineKeyboardButton(text='IP', callback_data='get_ip')]
-               ])
-    bot.sendMessage(chat_id, 'Server menu:', reply_markup=keyboard)
-
+    if auth(chat_id):
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                       [InlineKeyboardButton(text='Hostname',
+                           callback_data='hostname'),
+                        InlineKeyboardButton(text='IP', callback_data='get_ip')]
+                    ])
+        bot.sendMessage(chat_id, 'Server menu:', reply_markup=keyboard)
+        print(content_type, chat_type, chat_id)
+    else:
+        bot.sendMessage(chat_id, "Sorry, you don't have authorization")
 def cb_handle(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    if query_data == 'hostname':
-        res = commands.get_hostname()
-    elif query_data == 'get_ip':
-        res = commands.get_ip()
+    if auth(from_id):
+        if query_data == 'hostname':
+            res = commands.get_hostname()
+        elif query_data == 'get_ip':
+            res = commands.get_ip()
+        else:
+            print('Callback Query:', query_id, from_id, query_data)
+            bot.answerCallbackQuery(query_id, text='Got it')
+        bot.answerCallbackQuery(query_id, text=res)
+        print(query_id, from_id, query_data)
     else:
-        print('Callback Query:', query_id, from_id, query_data)
-        bot.answerCallbackQuery(query_id, text='Got it')
-    
-    bot.answerCallbackQuery(query_id, text=res)
-
-
+        bot.sendMessage(chat_id, "Sorry, you don't have authorization")
 
 TOKEN = config.TOKEN  # get token from command-line
 

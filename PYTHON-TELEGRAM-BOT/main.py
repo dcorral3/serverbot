4a1002fd4e8cf5ad37c2cffb19b633cc511f3a43
Diego del Corral <diegocorral.del@gmail.com>
2018-07-12 21:42:46 +0200

    deleted echo command

--- PYTHON-TELEGRAM-BOT/main.py
+++ PYTHON-TELEGRAM-BOT/main.py
@@ -8,9 +8,6 @@ dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', cmd.start)
 dispatcher.add_handler(start_handler)
 
-echo_handler = MessageHandler(Filters.text, cmd.echo)
-dispatcher.add_handler(echo_handler)
-
 button_handler = CallbackQueryHandler(cmd.button)
 dispatcher.add_handler(button_handler)
 

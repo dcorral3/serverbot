3790cac61cd9b088384c93e2f54ec0da045a3695
Diego del Corral <diegocorral.del@gmail.com>
2018-07-12 21:39:29 +0200

    PTB ip and host buttons

--- PYTHON-TELEGRAM-BOT/cmd.py
+++ PYTHON-TELEGRAM-BOT/cmd.py
@@ -1,7 +1,31 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import socket
+import requests
+import json
+
+keyboard = [[InlineKeyboardButton("Hostname", callback_data='hostname'),
+             InlineKeyboardButton("IP", callback_data='ip')],
+            [InlineKeyboardButton("Option 3", callback_data='3')]]
+reply_markup = InlineKeyboardMarkup(keyboard)
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Nene cara pene")
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+def button(bot, update):
+    query = update.callback_query
+    if query.data == "hostname":
+        res = hostname()
+    elif query.data == "ip":
+        res = ip()
+    else:
+        res = "default"
 
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    bot.send_message(chat_id=query.message.chat_id, text=res)
+    bot.answer_callback_query(query.id)
 
+def hostname():
+    return socket.gethostname()
+
+def ip():
+    res = requests.get('https://ipinfo.io/')
+    return json.loads(res.text)["ip"]
 
--- PYTHON-TELEGRAM-BOT/main.py
+++ PYTHON-TELEGRAM-BOT/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from config import TOKEN2
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import TOKEN
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler , MessageHandler, Filters
 import cmd
-updater = Updater(token=TOKEN2)
+updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', cmd.start)
@@ -11,4 +11,8 @@ dispatcher.add_handler(start_handler)
 echo_handler = MessageHandler(Filters.text, cmd.echo)
 dispatcher.add_handler(echo_handler)
 
+button_handler = CallbackQueryHandler(cmd.button)
+dispatcher.add_handler(button_handler)
+
 updater.start_polling()
+

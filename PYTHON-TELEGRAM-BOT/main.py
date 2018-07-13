08bc225fd8d04ac86544c7d22fa6676f75d48617
Diego del Corral <diegocorral.del@gmail.com>
2018-07-13 16:42:39 +0000

    reboot notifiaction when script is passed the -r argument

--- PYTHON-TELEGRAM-BOT/main.py
+++ PYTHON-TELEGRAM-BOT/main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-from config import TOKEN
+from config import TOKEN, admin_id
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler , MessageHandler, Filters
 import cmd
+import sys
+
+
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
+
+if len(sys.argv) > 1 and sys.argv[1]=="-r":
+    updater.bot.send_message(chat_id=admin_id, text="Reboot performed")
 
 start_handler = CommandHandler('start', cmd.start)
 dispatcher.add_handler(start_handler)

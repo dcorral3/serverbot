dfcb3dc28f8f6cec588ecc4e09ddb4f75fe5fcf8
Diego del Corral <diegocorral.del@gmail.com>
2018-07-16 14:22:22 +0000

    Update ip reboot options working

--- PYTHON-TELEGRAM-BOT/cmd.py
+++ PYTHON-TELEGRAM-BOT/cmd.py
@@ -1,12 +1,14 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import socket
+import os
 import requests
 import json
-from config import DNS_pass, hosts
+from config import DNS_pass, hosts, admin_id
 
 keyboard = [[InlineKeyboardButton("Hostname", callback_data='hostname'),
              InlineKeyboardButton("IP", callback_data='ip')],
-            [InlineKeyboardButton("Update IP", callback_data='update_ip')]]
+            [InlineKeyboardButton("Update IP", callback_data='update_ip'),
+             InlineKeyboardButton("Reboot", callback_data='reboot')]]
 reply_markup = InlineKeyboardMarkup(keyboard)
 
 def start(bot, update):
@@ -17,8 +19,12 @@ def button(bot, update):
         res = hostname()
     elif query.data == "ip":
         res = ip()
-    else:
+    elif query.data == "update_ip":
         res = update_ip()
+    elif query.data == "reboot":
+        res = reboot(query.message.chat_id)
+    else:
+        print("nothing to do")
 
     bot.send_message(chat_id=query.message.chat_id, text=res)
     bot.answer_callback_query(query.id)
@@ -36,3 +42,10 @@ def update_ip():
         res_url = requests.get('https://dynamicdns.park-your-domain.com/update?host='+ host +'&domain=diegoct.com&password='+ DNS_pass +'&ip=')
         res = res + host +": " + str(res_url.status_code)+"\n"
     return res
+
+def reboot(chat_id):
+    if str(chat_id) == admin_id:
+        os.system("sudo reboot")
+        return "reboot..."
+    else:
+        return "You are not authorized to do a reboot"
